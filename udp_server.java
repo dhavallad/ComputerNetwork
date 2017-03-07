bfe4f7f6248cafb512f3dfae12287dd315f849e0
dhavallad <dhavallad92@gmail.com>
2017-03-06 23:56:02 -0500

    Sample programming code for CN socket programming

---
+++ udp_server.java
@@ -0,0 +1,158 @@
+import java.io.*;
+import java.net.*;
+ 
+public class udp_server
+{
+    public static void main(String args[])
+    {
+        DatagramSocket sock = null;
+         
+        try
+        {
+            //1. creating a server socket, parameter is local port number
+            sock = new DatagramSocket(7777);
+             
+            //buffer to receive incoming data
+            byte[] buffer = new byte[65536];
+            DatagramPacket incoming = new DatagramPacket(buffer, buffer.length);
+             
+            //2. Wait for an incoming data
+            echo("Server socket created. Waiting for incoming data...");
+             
+            //communication loop
+            while(true)
+            {
+                sock.receive(incoming);
+                byte[] data = incoming.getData();
+                String s = new String(data, 0, incoming.getLength());
+                 
+                //echo the details of incoming data - client ip : client port - client message
+                echo(incoming.getAddress().getHostAddress() + " : " + incoming.getPort() + " - " + s);
+
+                s = "OK : " + eval(s);
+                DatagramPacket dp = new DatagramPacket(s.getBytes() , s.getBytes().length , incoming.getAddress() , incoming.getPort());
+                sock.send(dp);
+            }
+        }
+         
+        catch(IOException e)
+        {
+            System.err.println("IOException " + e);
+        }
+    }
+     
+    //simple function to echo data to terminal
+    public static void echo(String msg)
+    {
+        System.out.println(msg);
+    }
+
+    public static double eval(final String str) {
+        return new Object() {
+            int pos = -1, ch;
+
+            void nextChar() {
+                ch = (++pos < str.length()) ? str.charAt(pos) : -1;
+            }
+
+            boolean eat(int charToEat) {
+                while (ch == ' ')
+                    nextChar();
+                if (ch == charToEat) {
+                    nextChar();
+                    return true;
+                }
+                return false;
+            }
+
+            double parse() {
+                nextChar();
+                double x = parseExpression();
+                if (pos < str.length())
+                    throw new RuntimeException("Unexpected: " + (char) ch);
+                return x;
+            }
+
+            // Grammar:
+            // expression = term | expression `+` term | expression `-` term
+            // term = factor | term `*` factor | term `/` factor
+            // factor = `+` factor | `-` factor | `(` expression `)`
+            // | number | functionName factor | factor `^` factor
+
+            double parseExpression() {
+                double x = parseTerm();
+                for (;;) {
+                    if (eat('+'))
+                        x += parseTerm(); // addition
+                    else if (eat('-'))
+                        x -= parseTerm(); // subtraction
+                    else
+                        return x;
+                }
+            }
+
+            double parseTerm() {
+                double x = parseFactor();
+                for (;;) {
+                    if (eat('*'))
+                        x *= parseFactor(); // multiplication
+                    else if (eat('/'))
+                        x /= parseFactor(); // division
+                    else
+                        return x;
+                }
+            }
+
+            double parseFactor() {
+                if (eat('+'))
+                    return parseFactor(); // unary plus
+                if (eat('-'))
+                    return -parseFactor(); // unary minus
+
+                double x;
+                int startPos = this.pos;
+                if (eat('(')) { // parentheses
+                    x = parseExpression();
+                    eat(')');
+                } else if ((ch >= '0' && ch <= '9') || ch == '.') { // numbers
+                    while ((ch >= '0' && ch <= '9') || ch == '.')
+                        nextChar();
+                    x = Double.parseDouble(str.substring(startPos, this.pos));
+                } else if (ch >= 'a' && ch <= 'z') { // functions
+                    while (ch >= 'a' && ch <= 'z')
+                        nextChar();
+                    String func = str.substring(startPos, this.pos);
+                    x = parseFactor();
+                    if (func.equals("sqrt"))
+                        x = Math.sqrt(x);
+                    else if (func.equals("sin"))
+                        x = Math.sin(Math.toRadians(x));
+                    else if (func.equals("cos"))
+                        x = Math.cos(Math.toRadians(x));
+                    else if (func.equals("tan"))
+                        x = Math.tan(Math.toRadians(x));
+                    else
+                        throw new RuntimeException("Unknown function: " + func);
+                } else {
+                    throw new RuntimeException("Unexpected: " + (char) ch);
+                }
+
+                if (eat('^'))
+                    x = Math.pow(x, parseFactor()); // exponentiation
+
+                return x;
+            }
+        }.parse();
+    }
+
+
+
+
+
+
+
+
+
+
+
+}
